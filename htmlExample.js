1885c6ead4e6d951fa8abc3767a9101c1fd46e72
Fatima <en.fh90@gmail.com>
2018-02-20 21:31:08 +0200

    init

---
+++ htmlExample.js
@@ -0,0 +1,17 @@
+/*
+	1) create HTML web-page HOME.html then set the title to Toy Problem 
+	2) create another web-page favorite.html then add your five favorite picture 
+	3) create web-page Movie.html that includes an order list for your favorite 3 countries and an unordered 	
+		list for your best five movies
+	4) create web-page bio.html and add two paragraphs about you and your pair. 
+	5) if you finished the above, can you link these pages toghter? 
+
+		home.html
+		/|\
+	   / | favorite.html
+	  /  |  
+	 /  Movie.html
+	/
+	bio.html
+	
+*/
