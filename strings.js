c421493b482346ed09599659decc456707565692
shefaam <sewar.sas11@gmail.com>
2018-02-26 08:37:34 +0200

    edited

---
+++ strings.js
@@ -0,0 +1,14 @@
+// Write a function called characPosit, when provided with a letter, return its position in the alphabet.
+// 	Input :: "a"
+// 	Ouput :: "Position of alphabet: 1"
+	function characPosit(character){
+		//your code is here
+	}
+
+// Write a function called repeatStr which repeats the given
+// string string exactly n times.
+// repeatStr("hello",4) ==> 'hellohellohellohello'
+	
+	function repeatStr (n, s) {
+		//your code is here
+	}
---
+++ strings2.js
@@ -0,0 +1,19 @@
+//  Given a sequence of items and a specific item in that sequence, return the item
+// 	immediately following the item specified. If the item occurs more than once in a sequence,
+// 	return the item after the first occurence. This should work for a sequence of any type.
+// 	When the item isn't present or nothing follows it, the function should return null
+//  nextItem([1, 2, 3, 4, 5, 6, 7], 3) # 4
+//  nextItem("testing", "t") # "e"
+
+	function nextItem(items, elem){
+		hh
+	}
+
+//  We need a function that can transform a number into a string.
+// 	What ways of achieving this do you know?
+// 	numberToString(123); // returns '123';`   
+// 	numberToString(999); // returns '999';`
+
+	function numberToString(num) {
+	  //your code is here
+	}
